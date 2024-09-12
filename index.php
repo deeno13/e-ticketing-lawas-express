d9bd1a725479b74d06ba039930b618933c4bccff
Awang Hafizuddin <hafizxx13@gmail.com>
2024-09-12 23:51:35 +0800

    Style: Add basic styling for footer

--- index.php
+++ index.php
@@ -56,10 +56,40 @@ session_start();
 			</div>
 		</main>
 
-		<footer>
-			<div class="row">
-				<p>Copyright (c) 2023 - My Name</p>
+		<footer class="footer">
+			<div class="footer__content">
+				<div class="footer__brand">
+					<div class="footer__logo">
+						<h2>E-Ticketing Lawas Express</h2>
+					</div>
+					<span class="footer__copyright">Copyright © 2024 - Nurul Izzati</span>
+				</div>
+
+				<nav class="footer__nav">
+					<section class="footer__section">
+						<h3 class="footer__heading">Section</h3>
+						<ul class="footer__list">
+							<li><a href="#" class="footer__link">Link</a></li>
+							<li><a href="#" class="footer__link">Link</a></li>
+						</ul>
+					</section>
+					<section class="footer__section">
+						<h3 class="footer__heading">Section</h3>
+						<ul class="footer__list">
+							<li><a href="#" class="footer__link">Link</a></li>
+							<li><a href="#" class="footer__link">Link</a></li>
+						</ul>
+					</section>
+					<section class="footer__section">
+						<h3 class="footer__heading">Section</h3>
+						<ul class="footer__list">
+							<li><a href="#" class="footer__link">Link</a></li>
+							<li><a href="#" class="footer__link">Link</a></li>
+						</ul>
+					</section>
+				</nav>
 			</div>
 		</footer>
+
 	</body>
 </html>
