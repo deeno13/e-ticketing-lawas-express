4706ef9d7374132196e7f0f430d6e70fc622168d
Awang Hafizuddin <hafizxx13@gmail.com>
2024-09-30 22:44:30 +0800

    Style login page

--- pages/login.php
+++ pages/login.php
@@ -7,21 +7,52 @@
 		<title>Login</title>
 	</head>
 
-	<body>
+	<body class="body">
+		<header class="header">
+			<?php include("../includes/navbar.php"); ?>
+		</header>
+
 		<main>
-			<form action="../auth/login_action.php" method="post">
-				<h2>User Login</h2>
+			<div class="center">
+				<div class="login-modal">
+					<form action="../auth/login_action.php" method="post">
+						<div style="display: flex; align-items: center; margin-bottom: 24px;">
+							<i class="fa-solid fa-user fa-fw fa-lg" style="margin-right: 10px;"></i>
+							<h2>User Login</h2>
+						</div>
 
-				<label for="email">Email</label>
-				<input type="email" id="email" name="email" required>
+						<?php if (isset($error)) { ?>
+							<div class="form-error" style="display: flex; align-items: center;">
+								<i class="fa-solid fa-circle-exclamation" style="margin-right: 10px"></i>
+								<p><?php echo $error; ?></p>
+							</div>
+						<?php } ?>
 
-				<label for="password">Password</label>
-				<input type="password" id="password" name="password" required maxlength="8">
+						<div class="form-field">
+							<label for="email">Email</label>
+							<input type="email" id="email" name="email" required>
+						</div>
 
-				<button type="submit">Login</button>
-			</form>
+						<div class="form-field">
+							<label for="password">Password</label>
+							<input type="password" id="password" name="password" required maxlength="8">
+						</div>
 
-			<p>No account yet? <a href="register.php">Register here</a></p>
+						<div class="form-field">
+							<button type="submit">Login</button>
+						</div>
+						<!-- <button class="button" type="reset">Reset</button> -->
+					</form>
+
+					<div style="text-align: center;">
+						<p>No account yet? <a href="register.php">Register here</a></p>
+					</div>
+				</div>
+			</div>
 		</main>
+
+		<footer>
+			<?php include("../includes/footer.php"); ?>
+		</footer>
 	</body>
 </html>
