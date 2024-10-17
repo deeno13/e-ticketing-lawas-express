d7de42ad5168fba5fa34dde58607970b242c41b9
Awang Hafizuddin <hafizxx13@gmail.com>
2024-10-17 22:09:59 +0800

    Add contact page content

--- contact.php
+++ contact.php
@@ -7,6 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<link rel="stylesheet" href="stylesheets/main.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 		<title>Contact</title>
 	</head>
@@ -20,12 +21,69 @@
 			<h1 style="text-align: center;">Contact Us</h1>
 		</div>
 
-		<main class="main">
-			<div class="row">
-				<div class="col-right">
+		<main class="main" style="height: 100%;">
+      <div style="padding: 0 30rem 5rem 30rem;">
+        <div style="display: grid; grid-template-columns: repeat(6, 1fr); gap: 2rem;">
+          <div style="grid-column: 2 / 4; background-color: white; padding: 2rem; border: 1px solid #ccc; border-radius: 0.5rem">
+            <div style="display: flex; flex-direction: column; gap: 2.5rem;">
+              <div style="display: flex; flex-direction: column; gap: 1.5rem;">
+                <h2>Contact Info</h2>
+                <div>
+                  <i class="fa-solid fa-phone fa-fw" style="margin-right: 10px"></i>
+                  <span>+60 123 456 789</span>
+                </div>
+                <div>
+                  <i class="fa-solid fa-envelope fa-fw" style="margin-right: 10px"></i>
+                  <span>lawas_express@gmail.com</span>
+                </div>
+                <div>
+                  <i class="fa-solid fa-location-dot fa-fw" style="margin-right: 10px"></i>
+                  <span>Lawas, Sarawak</span>
+                </div>
+              </div>
 
-				</div>
-			</div>
+              <div style="display: flex; flex-direction: column; gap: 1.5rem;">
+                <h2>Operating Hours</h2>
+                <div>
+                  <i class="fa-solid fa-clock fa-fw" style="margin-right: 10px"></i>
+                  <span>Monday - Friday: 9:00 AM - 5:00 PM</span>
+                </div>
+                <div>
+                  <i class="fa-solid fa-clock fa-fw" style="margin-right: 10px"></i>
+                  <span>Saturday: 9:00 AM - 12:00 PM</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style="grid-column: 4 / 6; background-color: white; padding: 2rem; border: 1px solid #ccc; border-radius: 0.5rem">
+            <form action="contact_action.php" method="post">
+              <div class="form-field">
+                <label for="name">Name</label>
+                <input type="name" id="name" name="name" required>
+              </div>
+
+              <div class="form-field">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+              </div>
+
+              <div class="form-field">
+                <label for="subject">Subject</label>
+                <input type="text" id="subject" name="subject" required>
+              </div>
+
+              <div class="form-field">
+                <label for="message">Message</label>
+                <textarea id="message" name="message" rows="5" required></textarea>
+              </div>
+
+              <div class="form-field">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
 		</main>
 
     <?php include("includes/footer.php"); ?>
